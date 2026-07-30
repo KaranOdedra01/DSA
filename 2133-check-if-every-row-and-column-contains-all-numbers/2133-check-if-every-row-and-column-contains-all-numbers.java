827f46c24b7fded167d87fb75d079ae0303a443a
Karan Odedra <134261084+KaranOdedra01@users.noreply.github.com>
2026-07-30 23:00:56 +0530

    Time: 25 ms (42.98%), Space: 47.8 MB (72.08%) - LeetHub

--- 2133-check-if-every-row-and-column-contains-all-numbers/2133-check-if-every-row-and-column-contains-all-numbers.java
+++ 2133-check-if-every-row-and-column-contains-all-numbers/2133-check-if-every-row-and-column-contains-all-numbers.java
@@ -7,30 +7,24 @@ class Solution {
         for(int i = 0;i<m;i++){
             c= 0;
             for(int j = 0;j<m;j++){
-                if(1<= arr[i][j] && arr[i][j] <= n){
+                
                if(rm.contains(arr[i][j]))
                  return false;
                rm.add(arr[i][j]);
-                }else
-               {
-                return false;
-               }
                 
             }
+            
             rm.clear();
             for(int j = 0;j<n;j++){
-                if(1<= arr[j][i] && arr[j][i] <= n){
                 if(rm.contains(arr[j][i]))
                     return false;
                 rm.add(arr[j][i]);
-                }else{
-                    return false;
-                }
+                
             }
             rm.clear();
     }
         
-    return true;
     
+    return true;
 }
 }
