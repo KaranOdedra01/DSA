74f7c1495899c80a3dcf0f6d89e62204897e5722
Karan Odedra <134261084+KaranOdedra01@users.noreply.github.com>
2026-08-03 10:21:41 +0530

    Time: 2 ms (56.67%), Space: 42.9 MB (86.45%) - LeetHub

---
+++ 1748-sum-of-unique-elements/1748-sum-of-unique-elements.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int sumOfUnique(int[] nums) {
+        HashMap<Integer,Integer> un = new HashMap<>();
+        int c = 1; 
+
+        for(int i = 0;i<nums.length;i++){
+            if(un.containsKey(nums[i])){
+                un.put(nums[i],un.get(nums[i])+1);
+                // c++;
+            }
+            else{
+                un.put(nums[i],1);
+            }
+        }
+        int sum = 0;
+        for(Map.Entry<Integer,Integer> re : un.entrySet()){
+            if(re.getValue() == 1){
+                sum+=re.getKey();
+            }
+        }
+    return sum;
+
+        
+    }
+}
