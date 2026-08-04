32f5ebb0fa4fed8b45f08888650310f3950b748f
Karan Odedra <134261084+KaranOdedra01@users.noreply.github.com>
2026-08-04 07:55:22 +0530

    Time: 0 ms (100%), Space: 43.2 MB (19.54%) - LeetHub

--- 1748-sum-of-unique-elements/1748-sum-of-unique-elements.java
+++ 1748-sum-of-unique-elements/1748-sum-of-unique-elements.java
@@ -1,24 +1,37 @@
 class Solution {
     public int sumOfUnique(int[] nums) {
-        HashMap<Integer,Integer> un = new HashMap<>();
-        int c = 1; 
+    //     HashMap<Integer,Integer> un = new HashMap<>();
+    //     int c = 1; 
 
-        for(int i = 0;i<nums.length;i++){
-            if(un.containsKey(nums[i])){
-                un.put(nums[i],un.get(nums[i])+1);
-                // c++;
-            }
-            else{
-                un.put(nums[i],1);
-            }
-        }
-        int sum = 0;
-        for(Map.Entry<Integer,Integer> re : un.entrySet()){
-            if(re.getValue() == 1){
-                sum+=re.getKey();
-            }
-        }
+    //     for(int i = 0;i<nums.length;i++){
+    //         if(un.containsKey(nums[i])){
+    //             un.put(nums[i],un.get(nums[i])+1);
+    //             // c++;
+    //         }
+    //         else{
+    //             un.put(nums[i],1);
+    //         }
+    //     }
+    //     int sum = 0;
+    //     for(Map.Entry<Integer,Integer> re : un.entrySet()){
+    //         if(re.getValue() == 1){
+    //             sum+=re.getKey();
+    //         }
+    //     }
+    // return sum;
+
+
+    int[] freq = new int[101];
+
+    for(int i :nums){
+        freq[i]++;
+    }
+    int sum = 0;
+    for(int i = 0;i<101;i++){
+        if(freq[i] == 1) sum += i;
+    }
     return sum;
+
 
         
     }
