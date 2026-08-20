903a512d41774ba07312e32e1296709a2d17f258
Karan Odedra <134261084+KaranOdedra01@users.noreply.github.com>
2026-08-20 09:05:21 +0530

    Time: 1 ms (100%), Space: 44.5 MB (88.2%) - LeetHub

--- 2864-maximum-odd-binary-number/2864-maximum-odd-binary-number.java
+++ 2864-maximum-odd-binary-number/2864-maximum-odd-binary-number.java
@@ -1,26 +1,53 @@
 class Solution {
     public String maximumOddBinaryNumber(String s) {
-        int c = 0;
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < s.length(); i++) {
-            if (s.charAt(i) == '1') {
-                c++;
+        int i = 0;
+        int j = s.length() - 1;
+
+        char[] arr = s.toCharArray();
+
+    
+        while (i <= j) {
+            if(arr[i]=='1'){
+                i++;
+            }
+            else if(arr[j] == '0'){
+                j--;
+            }
+           else  if(arr[i] == '0' && arr[j] == '1' && i < j){
+                char temp = arr[j];
+                arr[j] = arr[i];
+                arr[i] = temp;
+                i++;
+                j--;
             }
         }
-        if (c == 0) {
-            return s;
-        }
-        // c = c - 1;
-        for (int i = 0; i < s.length()-1; i++) {
-            if (c > 1) {
-                sb.append('1');
-                c--;
-            }
-            else{
-                sb.append('0');
-            }
-        }
-        sb.append('1');
-        return sb.toString();
+        arr[j] = '0';
+        arr[s.length()-1] = '1';
+
+        return new String(arr);
+
+        // int c = 0;
+        // StringBuilder sb = new StringBuilder();
+        // for (int i = 0; i < s.length(); i++) {
+        //     if (s.charAt(i) == '1') {
+        //         c++;
+        //     }
+        // }
+        // if (c == 0) {
+        //     return s;
+        // }
+        // // c = c - 1;
+        // for (int i = 0; i < s.length()-1; i++) {
+        //     if (c > 1) {
+        //         sb.append('1');
+        //         c--;
+        //     }
+        //     else{
+        //         sb.append('0');
+        //     }
+        // }
+        // sb.append('1');
+        // return sb.toString();
+
     }
 }
