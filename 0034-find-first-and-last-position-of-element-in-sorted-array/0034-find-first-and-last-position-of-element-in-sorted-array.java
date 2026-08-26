96c5cf9a36aa5c8797f3d00643c7bd42e2ea7c6a
Karan Odedra <134261084+KaranOdedra01@users.noreply.github.com>
2026-08-26 10:04:24 +0530

    Time: 0 ms (100%), Space: 48 MB (71.88%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,47 +1,40 @@
 import java.util.*;
 
 class Solution {
-    public int[] searchRange(int[] nums, int target) {
+    int binary(int[] nums,int target,boolean occur){
+
         int low = 0;
         int high = nums.length - 1;
+
         int ans = -1;
-        while (low <= high) {
-            int mid = low + (high - low) / 2;
-
-            if (nums[mid] == target) {
-                ans = mid;
-                high = mid-1;
-            }
-
-            else if (nums[mid] < target) {
-                low = mid + 1;
-            } else {
-                high = mid - 1;
-            }
-        }
-        if (ans == -1) {
-            return new int[] { -1, -1 };
-        }
-        int[] res = new int[2];
-        res[0] = ans;
-        low = ans;
-        high = nums.length - 1;
 
         while(low <= high){
-            int mid = low + (high - low) / 2;
+            int mid = low + (high - low)/2;
             if(nums[mid] == target){
                 ans = mid;
-                low = mid+1;
-            }
-            else if (nums[mid] < target) {
-                low = mid + 1;
-            } else {
+                if(occur){
                 high = mid - 1;
+                }
+                else{
+                    low = mid + 1;
+                }
+            }
+            else if(nums[mid] < target){
+                low = mid + 1;
+            }
+            else{
+                high = mid -1;
             }
         }
+        return ans;
 
-        res[1] = ans;
+    }
 
-        return res;
+    public int[] searchRange(int[] nums, int target) {
+
+            int first = binary(nums,target,true);
+            int second = binary(nums,target,false);
+            return new int[] {first,second};
+        
     }
 }
